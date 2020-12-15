c256dc64b801f2b807eb3151bcbe860910a37450
yinhaiying <yinhaiying@58.com>
2020-12-15 12:58:44 +0800

    拆分路由

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import  Koa from "koa";
 import  Router from "koa-router";
-
+import  users from "./routes/api/users";
 const app = new Koa();
-
 const router = new Router();
 
-router.get("/",async(ctx) => {
-    ctx.body = "hello,"
-});
 
-app.use(router.routes());
+router.use("/api/users",users.routes());
 
+
+app.use(router.routes()).use(router.allowedMethods());;
 const port = 3000;
 app.listen(port,() => {
     console.log(`server runnint on port ${port}`)
