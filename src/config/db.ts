a34771ce8995725d7f2037c94c44b434f06a2554
yinhaiying <yinhaiying@58.com>
2020-12-15 16:28:48 +0800

    使用mongoose和免费的数据库进行连接

---
+++ src/config/db.ts
@@ -0,0 +1,5 @@
+const dbConfig = {
+    "mongoURI": 'mongodb+srv://haiying:aaa123456@cluster0.pqdde.mongodb.net/test' 
+}
+
+export default dbConfig;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import  Koa from "koa";
 import  Router from "koa-router";
 import  users from "./routes/api/users";
+import mongoose from "mongoose";
+import dbConfig from "./config/db";
 const app = new Koa();
 const router = new Router();
+
+
+
+mongoose.connect(dbConfig.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('mongodb 连接成功');
+}).catch((err) => {
+    console.log('mongodb连接失败:' + err);
+})
+
 
 
 router.use("/api/users",users.routes());
